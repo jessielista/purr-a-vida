0664535f3c4d773988810fc6ee140dd5329489fd
jessielista <jmgonzalez8282@gmail.com>
2021-12-07 15:51:32 -0800

    adding items and props

--- bakery-app/src/App.js
+++ bakery-app/src/App.js
@@ -1,31 +1,52 @@
+// import { BrowserRouter } from 'react-router-dom'
+
+const items = [
+  {
+    id: 1,
+    title: 'Bread',
+    img: 'https://i.imgur.com/NmhFVwam.jpg',
+    price: 2.34,
+    description: 'Product description goes here.',
+  },
+  {
+    id: 2,
+    title: 'Cake',
+    img: 'https://i.imgur.com/nUiAItDm.jpg',
+    price: 24.34,
+    description: 'Product description goes here.',
+  },
+  {
+    id: 3,
+    title: 'Drinks',
+    img: 'https://i.imgur.com/tluGaY4m.jpg',
+    price: 4.34,
+    description: 'Product description goes here.',
+  },
+]
+
 function BakeryItems() {
   return (
     <div className='bakerylist'>
-      <BakedGood food='concha' />
-      <BakedGood />
-      <BakedGood />
-      <BakedGood />
-      <BakedGood />
-      <BakedGood />
+      {items.map((item) => {
+        return <BakedGood key={item.id} {...item}></BakedGood>
+      })}
     </div>
   )
 }
-const title = 'Bread #2'
-const price = 2.34
-const description = 'Product description goes here.'
-const img = 'https://i.imgur.com/NmhFVwam.jpg'
 
 const BakedGood = (props) => {
   console.log(props)
+  const { img, title, price, description } = props
+
   return (
     <div className='bakery-item'>
       <img src={img} alt='' />
       <h1>{title}</h1>
       <h3>{price}</h3>
       <h4>{description}</h4>
-      <p>{props.food}</p>
     </div>
   )
 }
 
+// add base url & fetch the backend
 export default BakeryItems
