1ffba84e5796f1245c029efeff61213383720d6b
jessielista <jmgonzalez8282@gmail.com>
2021-12-07 19:14:39 -0800

    added buttons

--- bakery-app/src/App.js
+++ bakery-app/src/App.js
@@ -37,6 +37,16 @@ function BakeryItems() {
 const BakedGood = (props) => {
   console.log(props)
   const { img, title, price, description } = props
+  const clickHandler1 = (e) => {
+    alert('Your item has been added')
+    console.log('item added')
+    console.log(e)
+    console.log(e.target)
+  }
+  const clickHandler2 = () => {
+    alert('Your item has been removed')
+    console.log('item removed')
+  }
 
   return (
     <div className='bakery-item'>
@@ -44,6 +54,12 @@ const BakedGood = (props) => {
       <h1>{title}</h1>
       <h3>{price}</h3>
       <h4>{description}</h4>
+      <button type='button' onClick={clickHandler1}>
+        Add Item to Cart
+      </button>
+      <button type='button' onClick={clickHandler2}>
+        Remove Item from Cart
+      </button>
     </div>
   )
 }
